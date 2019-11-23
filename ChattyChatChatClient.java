836b85a28f7fe88788acd615676225cd72483881
Colin Mills <cgm71@georgetown.edu>
2019-11-22 21:10:34 -0500

    implement Chattys

---
+++ ChattyChatChatClient.java
@@ -0,0 +1,74 @@
+public class ChattyChatChatClient {
+
+    protected String name;
+
+    /**
+     * Populates the shapeDefinitions vector with
+     * some definitions of shapes.  You'll need to implement
+     * the convertDescriptionsToShapes function to actually convert
+     * these descriptions to useful shapes.
+     *
+     * @param filename the file containing the definitions of the shapes
+     */
+    ChattyChatChatClient() {
+
+    }//END ChattyChatChat()
+
+    /**
+     * Populates the shapeDefinitions vector with
+     * some definitions of shapes.  You'll need to implement
+     * the convertDescriptionsToShapes function to actually convert
+     * these descriptions to useful shapes.
+     *
+     * @param filename the file containing the definitions of the shapes
+     */
+    ChattyChatChatClient(String nickName) {
+
+    }//END ChattyChatChat(nickName)
+
+
+    /**
+     *
+     * @param args command line arguments given by user
+     */
+    public static void main(String[] args) {
+
+    }//END main
+
+    /**
+     * Computes the sum of the shapes' areas, where the shapes
+     * are from the shapes list
+     * @return the sum of the shapes' areas
+     */
+    public void sendNickName() {
+
+    }//End sendNickName
+
+    /**
+     * Computes the sum of the shapes' areas, where the shapes
+     * are from the shapes list
+     * @return the sum of the shapes' areas
+     */
+    public void setNickName(myName) {
+
+    }//End setNickName
+
+
+    /**
+     * Computes the sum of the shapes' perimeters, where the shapes
+     * are from the shapes list
+     * @return the sum of the shapes' perimeters
+     */
+    public void sendNormalMessage(String message) {
+
+    }//END sendNormalMessage
+
+    public void sendPrivate(String name, String msg) {
+
+    }//END sendPrivate
+
+    public void exit() {
+
+    }//END exit()
+
+}
---
+++ ChattyChatChatServer.java
@@ -0,0 +1,54 @@
+public class ChattyChatChatServer {
+
+    protected String portNumber;
+    protected String serverName;
+
+
+    /**
+     * Populates the shapeDefinitions vector with
+     * some definitions of shapes.  You'll need to implement
+     * the convertDescriptionsToShapes function to actually convert
+     * these descriptions to useful shapes.
+     *
+     * @param filename the file containing the definitions of the shapes
+     */
+    ChattyChatChatServer(String portNum, String servName) {
+
+    }//END ChattyChatChat(portNum, ServNaMe)
+
+
+    /**
+     *
+     * @param args command line arguments given by user
+     */
+    public static void main(String[] args) {
+
+    }//END main
+
+
+
+    /**
+     * Computes the sum of the shapes' areas, where the shapes
+     * are from the shapes list
+     * @return the sum of the shapes' areas
+     */
+    public void clientHandler() {
+
+    }//End clientHandler
+
+
+
+    /**
+     * Computes the sum of the shapes' perimeters, where the shapes
+     * are from the shapes list
+     * @return the sum of the shapes' perimeters
+     */
+    public void newThread(Runnable myRun) {
+
+    }//END newThread
+
+    public void newRun() {
+
+    }//END newRun
+
+}
