b639d3077de66289d4619fcb37bdc7d1d39aee3e
Colin Mills <cgm71@georgetown.edu>
2019-11-24 19:08:56 -0500

    Added javadoc and comments

--- ChattyChatChatClient.java
+++ ChattyChatChatClient.java
@@ -8,9 +8,12 @@ import java.util.concurrent.TimeUnit;
 public class ChattyChatChatClient {
 
     /**
-
+     * Connects client to server and waits for client input to send to server
+     * passes listening responsibilities to new thread
+     * @param args should have a hostName and a port number as command line arguments
      */
     public static void main(String[] args) {
+        //All objects that main needs to use
         String hostName = args[0];
         String userInput = "";
         String[] parsedInput = null;
@@ -24,12 +27,14 @@ public class ChattyChatChatClient {
         Runnable clientRun = null;
 
         try {
+            //Connect to server
             socket = new Socket(hostName, port);
             System.out.println("Connection successful, Type \"/quit\" to quit, \"dm\" followed by a name and message to dm, or just a message for all.");
+            //Initialize readers/writers for communication
             readerIn = new BufferedReader(new InputStreamReader(System.in));
             serverIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             socketOut = new PrintStream(socket.getOutputStream(), true);
-
+            //Pass listening responsibilities elsewhere
             clientRun = new ChattyClientRunnable(serverIn);
             clientThread = new Thread(clientRun);
             clientThread.start();
@@ -43,10 +48,8 @@ public class ChattyChatChatClient {
                     runServer = false;
                     System.out.println("Terminating connection to server");
                 }//END if
-
+                //Will send a message to the server regardless, but will end loop if quit is the msg
                 socketOut.println(userInput);
-                //System.out.println("Sent: " + userInput);
-
             }//END while (runServer)
         }
         catch (IOException e) {
@@ -61,42 +64,50 @@ public class ChattyChatChatClient {
         }
         finally {
             try {
+                //Make sure everything is closed
                 socket.close();
                 readerIn.close();
                 socketOut.close();
                 System.out.println("Connection successfully terminated");
             }//END try close
             catch (Exception e) {
-
             }//END catch close exception
-
         }//END finally
-
-
     }//END main
 
+    /**
+     * Listens to server until connection is stopped by main
+     */
     public static class ChattyClientRunnable implements Runnable {
         protected BufferedReader socketIn;
 
+        /**
+         * initializes where to listen from server
+         * @param socketIn Buffered Reader connected to server through socket
+         */
         ChattyClientRunnable(BufferedReader socketIn) {
             this.socketIn = socketIn;
         }// END ChattyThread()
 
+        /**
+         * Listens as long as there is a connection and outputs results
+         */
         @Override
         public void run() {
+
             String inMessage = "";
             Boolean done = false;
 
             while (!done) {
-
                 try {
                     inMessage = socketIn.readLine();
-
+                    //If there is anything it'll get output
                     if (!(inMessage.equals(""))) {
                         System.out.println(inMessage);
                     }//END else
                 }//END try
                 catch (IOException e) {
+                    //Main has ended connection or other issue has come up, either way this runnable isn't needed
                     System.out.println("Connection interrupted... Ending runnable.");
                     done = true;
                 }//END IO exception
@@ -106,10 +117,7 @@ public class ChattyChatChatClient {
                     System.out.println(e.getCause());
                     done = true;
                 }//END unknown exception
-
             }//END while
-
         }//END run
     }//END ChattyClientRunnable
-
-}
+}//END ChattyChatChatClient
--- ChattyChatChatServer.java
+++ ChattyChatChatServer.java
@@ -6,16 +6,19 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Vector;
 
-//import java.io.IOException.SocketException;
+/**
+ * @author Colin Mills
+ */
 
 
 public class ChattyChatChatServer {
 
     /**
-     *
+     * Main waits at port for connections and then passes clients to new thread
      * @param args command line arguments given by user
      */
     public static void main(String[] args) {
+        //Initialize all needed objects
         ServerSocket portListener = null;
         boolean runServer = true;
         String stringPort = args[0];
@@ -28,6 +31,7 @@ public class ChattyChatChatServer {
         Runnable myRun = null;
 
         try {
+            //First bind to port
             System.out.println("Binding to port " + stringPort + " If you wish to terminate type \"exit\"");
             portListener = new ServerSocket(intPort);
         } catch (IOException e) {
@@ -41,12 +45,15 @@ public class ChattyChatChatServer {
 
         while (runServer) {
             try {
+                //Waits until connection is accepted
                 System.out.println("Ready for a new client.");
                 socket = portListener.accept();
                 myRun = new ChattyServerRunnable(socket, list);
                 myThread = new Thread(myRun);
+                //Passes client handling responsibilities to new thread
                 myThread.start();
-            } catch (IOException e) {
+            }
+            catch (IOException e) {
                 System.out.println("Error establishing socket");
                 System.out.println(e.fillInStackTrace());
                 System.out.println(e.getCause());
@@ -58,20 +65,6 @@ public class ChattyChatChatServer {
                 System.out.println(e.getCause());
                 runServer = false;
             }//END unknown error
-
-            /*try {
-                serverSideInput = readerIn.readLine();
-
-                if (serverSideInput.equals("exit") || serverSideInput.equals("Exit")) {
-                    runServer = false;
-                }//END if exit
-            } catch (IOException e) {
-                System.out.println("Error with server side input");
-                System.out.println(e.fillInStackTrace());
-                System.out.println(e.getCause());
-            }
-*/
-
         }//END while loop
         try {
             System.out.println("Closing connection");
@@ -83,51 +76,89 @@ public class ChattyChatChatServer {
                 }//END try close
                 catch (Exception e) {/* Nothing */}//END catch close exception
         }//END Finally
-
     }//END main
 
+    /**
+     * Client knows its own name and how to send to that client
+     */
     public static class Client {
         protected String nickName;
         protected PrintWriter socketOut;
 
+        /**
+         * Initialize client
+         * @param out is Print Writer for desired socket connection
+         */
         Client(PrintWriter out) {
             nickName = " ";
             socketOut = out;
         } //END constructor
 
+        /**
+         * Sets nickname for DM purposes
+         * @param newName value passed by client handler for what client name should be
+         */
         public void setNickName (String newName) {
             nickName = newName;
         }//END setNickName
 
+        /**
+         * Lets client handler find person with nickName
+         * @return returns nickName
+         */
         public String getNickName() {
             return nickName;
         }//END get NickName
 
+        /**
+         * Uses print writer to send message
+         * @param msg string of what should be sent to client
+         */
         public void sendMsg (String msg) {
             socketOut.println(msg);
         }//END sendMsg
     } //END Client
 
+    /**
+     * Keeps track of all clients for server and delegates what Clients should do
+     */
     public static class ClientList {
         protected Vector<Client> clientList;
 
+        /**
+         * Initialize new vector of type client
+         */
         ClientList() {
             clientList = new Vector<>();
         }//END constructor
 
+        /**
+         * adds new client.. called by ChattyRunnable
+         * @param newClient new client that has been connected
+         */
         public void addClient(Client newClient) {
             clientList.add(newClient);
         }//END addClient
 
+        /**
+         * Tells each client to send this message
+         * @param msg String sent to each connected client
+         */
         public void sendPublic(String msg) {
             for(Client c: clientList) {
                 c.sendMsg(msg);
             }//END for Client
         }//END sendPublic
 
+        /**
+         * Sends private DM
+         * @param name Only sent to matching names
+         * @param msg from client
+         */
         public void sendDM(String name, String msg) {
             for(Client c: clientList) {
                 if(name.equals(c.getNickName())) {
+                    //Only send if matching
                     c.sendMsg(msg);
                 }//END if
             }//END for Client
@@ -135,17 +166,27 @@ public class ChattyChatChatServer {
 
     }//END ClientList
 
+    /**
+     * Runnable that gets started in a new thread and is responsible for interaction with a single client
+     */
     public static class ChattyServerRunnable implements Runnable {
         protected Socket socket;
         protected BufferedReader in;
         protected Client myClient;
         protected ClientList list;
 
+        /**
+         * Initializes everything this thread needs to know and tells client list
+         * @param sock socket connection used to establish input and output readers/writers
+         * @param list clientList passed in so new client can be added to same one
+         */
         ChattyServerRunnable(Socket sock, ClientList list) {
             try {
                 socket = sock;
+                //Create reader/writer from socket connection
                 in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 PrintWriter socketOut = new PrintWriter(socket.getOutputStream(), true);
+                //Create and add client to list
                 myClient = new Client(socketOut);
                 list.addClient(myClient);
                 this.list = list;
@@ -157,11 +198,14 @@ public class ChattyChatChatServer {
             }//
         }// END ChattyServerRunnable()
 
+        /**
+         * Stays running as long as client is connected
+         * Delegates all responsibilities to client handler when necessary
+         */
         @Override
         public void run() {
 
             boolean done = false;
-
             System.out.println("Successfully passed client to their own thread. Waiting for client interaction.");
 
                 try {
@@ -170,7 +214,7 @@ public class ChattyChatChatServer {
                         String response = in.readLine();
                         System.out.println("Received: " + response);
                         parsedResponse = response.split(" ");
-                        //System.out.println("parsed first word: " + parsedResponse[0]);
+                        //Gets response and splits it up for evaluation
 
                         if (parsedResponse[0].equals("/quit")) {
                             System.out.println("Ending connection with a client");
@@ -186,6 +230,7 @@ public class ChattyChatChatServer {
                             System.out.println("Sending DM to " + name);
                             for (int i = 2; i < parsedResponse.length; i++) {
                                 if (i == 2) {
+                                    //So there isn't a leading space
                                     message += parsedResponse[i];
                                 }//END first time
                                 else {
@@ -199,6 +244,7 @@ public class ChattyChatChatServer {
                             System.out.println("Sending message to all");
                             for (int i = 0; i < parsedResponse.length; i++) {
                                 if (i == 0) {
+                                    //So there isn't a leading space
                                     message += parsedResponse[i];
                                 }//First case
                                 else {
